a5010c1ae2fed1db353801189fd576ea4500cf9a
rohanchandra30 <rohanchandra30@gmail.com>
2017-05-03 01:49:59 -0400

    Finsihing Question 2 code - Debugging Required

--- rchandr1_hw3/Code/Question2.m
+++ rchandr1_hw3/Code/Question2.m
@@ -1,4 +1,4 @@
-% Implementing Algorithm given on page 76 of Eigenface paper. 
+% Implementing Algorithm given on page 76 of Eigenface paper.
 
 clc
 clear all
@@ -8,32 +8,87 @@ index = 1;
 % Read in Training Images
 for i = 1:40
     path1 = sprintf('../Images/Dataset/Train/s%d', i);
-  
+    
     for j = 1:4
         path2 = sprintf('%d.pgm', j);
         filename = fullfile(path1, path2);
         image{index} = im2double(imread(filename));
-        im_vectors(:,index) = image{index}(:);
+        train_vectors(:,index) = image{index}(:);
         index = index + 1;
     end
     
 end
 M = index - 1;
 % Computing mean face
-chi = sum(im_vectors, 2)./M;
+chi = sum(train_vectors, 2)./M;
 
-% STEP 2: Computing phi
-phi = bsxfun(@minus, im_vectors, chi);
+% STEP 2:
+% Computing phi
+phi = bsxfun(@minus, train_vectors, chi);
 A = phi;
 
-% STEP 2: Computing L matrix
+
+% Computing L matrix
 L = A'*A;
 
-% STEP 2: Compute M' = 40 largest eigenvectors of L matrix
+% Compute M' = 40 largest eigenvectors of L matrix
 [V, D] = eig(L);
 V = V(:, end - 39:end);
 
-% STEP 3: Combine according to equation 6
+% STEP 3:
+% Combine according to equation 6
 U =  phi*V;
 
-% STEP 4: 
+% STEP 4:
+% Compute weigth vectors for all input faces
+W = U'*phi;
+
+for i = 1:40
+    Omega_train(:, i) = W(:, 4*i-3) + W(:, 4*i-2) + W(:, 4*i-1) + W(:, 4*i)/4;
+    
+end
+
+
+% STEP 5:
+% Compute the new Omega vector by computing weights for new face as w =
+% U'*(gamma - chi)
+
+% Read in test data
+index = 1;
+for i = 1:40
+    path1 = sprintf('../Images/Dataset/Test/s%d', i);
+    
+    for j = 5:10
+        path2 = sprintf('%d.pgm', j);
+        filename = fullfile(path1, path2);
+        image_test{index} = im2double(imread(filename));
+        test_vectors(:,index) = image_test{index}(:);
+        index = index + 1;
+    end
+    
+end
+
+% Normalize
+M = index - 1;
+chi = sum(test_vectors, 2)./M;
+phi_test = bsxfun(@minus, test_vectors, chi);
+W_test = U'*phi_test;
+% Compute average faces for test data
+for i = 1:40
+    Omega_test(:, i) = (W_test(:, 6*i-5) + W_test(:, 6*i-4) + ...
+        W_test(:, 6*i-3) + W_test(:, 6*i - 2) +...
+        W_test(:, 6*i - 1) + W_test(:, 6*i))/4;
+    
+end
+
+% Compute Class
+for i = 1:40
+    for j = 1:40
+%    Compute Distance
+   eps(j) =  norm(Omega_train(:,j) - Omega_test(:,i)).^2;
+    end
+    % Find minimum
+class(i) = find(eps == min(eps));
+end
+
+Accuracy = (length(find(class - [1:40] == 0)))/.4
